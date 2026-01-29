7d9d58ee6e896e479e019dbce244f53f3e38fe0b
rendinura <rendinuragustin@gmail.com>
2026-01-29 13:26:58 +0700

    fix: update next config for images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hwztchapter.dramaboxdb.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hwztvideo.dramaboxdb.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
